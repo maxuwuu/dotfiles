6fbb199f47e7a1eeb4dd66ff816cbce053d66936
Max <emirshn000@gmail.com>
2025-05-04 14:25:13 +0300

    )(

--- İ3/rofi/rofi-theme-from-nitrogen.sh
+++ İ3/rofi/rofi-theme-from-nitrogen.sh
@@ -11,7 +11,7 @@ wal -i "$WALLPAPER" > /dev/null
 BACKGROUND=$(sed -n '1p' ~/.cache/wal/colors)
 FOREGROUND=$(sed -n '8p' ~/.cache/wal/colors)
 ACCENT=$(sed -n '2p' ~/.cache/wal/colors)
-
+ROFI_CONFIG = "HOME/.config/rofi/config.rasi"
 cat > "$ROFI_CONFIG" <<EOF
 * {
     background-color: $BACKGROUND;
