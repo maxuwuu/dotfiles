f58b53248694f5ca3c365a20a5ef4b4d448531c9
Max <emirshn000@gmail.com>
2025-05-04 14:10:08 +0300

    :_-

--- İ3/rofi/rofi-theme-from-nitrogen.sh
+++ İ3/rofi/rofi-theme-from-nitrogen.sh
@@ -23,4 +23,56 @@ cat > "$ROFI_CONFIG" <<EOF
     urgent-background: $ACCENT;
     bordercolor: $ACCENT;
 }
+
+#window {
+    background-color: $BACKGROUND;
+    border:           3;
+    border-radius:    6;
+    border-color:     $ACCENT;
+    padding:          15;
+}
+
+#listview {
+    fixed-height: 0;
+    border:       0px;
+    border-color: $ACCENT;
+    spacing:      2px ;
+    scrollbar:    false;
+    padding:      2px 0px 0px ;
+}
+
+#element {
+    border:  0;
+    padding: 3px ;
+}
+
+#element.normal.normal {
+    background-color: $BACKGROUND;
+    text-color:       $FOREGROUND;
+}
+
+#element.normal.active {
+    background-color: $ACCENT;
+    text-color:       $FOREGROUND;
+}
+
+#element.selected.normal {
+    background-color: $ACCENT;
+    text-color:       $FOREGROUND;
+}
+
+#scrollbar {
+    width:        2px ;
+    border:       0;
+    handle-width: 8px ;
+    padding:      0;
+}
+
+#inputbar {
+    spacing:    0;
+    text-color: $FOREGROUND;
+    padding:    1px ;
+}
 EOF
+
+echo "Rofi teması güncellendi: $ROFI_CONFIG"
